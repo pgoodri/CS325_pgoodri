7952b41dd555b76b1c3e9e138d89e68cc5e2d12f
pgoodri <paigegoodrich03@gmail.com>
2024-01-26 13:18:13 -0600

    added 3 print lines to hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,6 @@
 print("Hello, world.")
 print("My name is Paige Goodrich.")
+
+print("I have a Major in Computer Science and a Minor in Business Administration.")
+print("This is my second line.")
+print("This is my third line.")
